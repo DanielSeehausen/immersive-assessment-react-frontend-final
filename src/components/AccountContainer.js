0b70cb4f3ee3141108ea0044a6db31f16156eef6
Daniel Seehausen <Daniel.Seehausen@gmail.com>
2017-07-12 14:56:35 -0400

    updated solution

--- src/components/AccountContainer.js
+++ src/components/AccountContainer.js
@@ -2,36 +2,7 @@ import React, { Component } from 'react'
 import TransactionsList from './TransactionsList'
 import CategorySelector from './CategorySelector'
 import categories from '../categories'
-const seedData = [
-  {
-  id: 1,
-  posted_at: "2017-02-28 11:00:00",
-  description: "Leather Pants, Gap co.",
-  category: "Fashion",
-  amount: -20000
-  },
-  {
-  id: 2,
-  posted_at: "2017-02-29 10:30:00",
-  description: "Paycheck from Bob's Burgers",
-  category: "Income",
-  amount: 100000
-  },
-  {
-  id: 3,
-  posted_at: "2017-05-24 10:53:00",
-  description: "'Pair Programming Illuminated' by Laurie Williams and Robert Kessler",
-  category: "Entertainment",
-  amount: -1498
-  },
-  {
-  id: 4,
-  posted_at: "2017-05-24 08:52:00",
-  description: "Medium Iced Cold Brew, Gregory's Coffee",
-  category: "Food",
-  amount: -365
-  }
-]
+import seedData from '../seedData'
 
 class AccountContainer extends Component {
   constructor() {
@@ -51,7 +22,7 @@ class AccountContainer extends Component {
     var newCats = new Set([...this.state.activeCats])
     val === "on" && newCats.delete(cat) || newCats.add(cat) // jankINESss TO THE MAX!!
     newCats.delete("All")
-    this.shouldDisplayAll() && newCats = new Set(["All"]) // jankmasterflex
+    this.shouldDisplayAll(newCats, cat) && (newCats = new Set(["All"])) // jankmasterflex. 
     this.setState({ activeCats: newCats })
   }
 
